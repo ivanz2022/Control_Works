cb1cd30067a26db85b19912f307be25cd648a002
ivanz2022 <verstka.dtp.pro@gmail.com>
2022-07-27 15:28:42 +0300

    Add basic code in project

--- Control_Work_1/Program.cs
+++ Control_Work_1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] currentArray = {"Rus", "178", "Denark", "+)", "0"};
+int countElements = 0;
+
+for (int i = 0; i < currentArray.Length; i++)
+{
+    if (currentArray[i].Length <= 3)
+    {
+        countElements++;
+    } 
+}
+
+if (countElements == 0)
+{
+    Console.WriteLine("Строк длинной 3 или менее символов в массиве нет.");
+}
+else
+{
+    string[] newArray = new string [countElements];
+    string temp = String.Empty;
+    int newSize = 0;
+
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if (currentArray[i].Length <= 3)
+        {
+            temp = currentArray[i];
+            newArray[newSize] = temp;
+            newSize++;  
+        }      
+    }
+}
